75825dee26fbb8fa02c09b06b9d0c81977caec04
KiraArensky <kshi.txt@yandex.ru>
2025-03-11 01:24:56 +0500

    с

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+
+# Main index.html template
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Personal greeting pages
+@app.route('/personal/<name>.html')
+def personal_greeting(name):
+    return render_template(f'personal/{name}.html')
+
+
+# Serve static images
+@app.route('/pic/<filename>')
+def serve_image(filename):
+    return send_from_directory('static/pic', filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
